2b308674369363f63acbbbe476d41ca58172914b
Ron Kurr <rkurr@overstock.com>
2022-01-04 22:01:24 +0000

    Play with constructor validation.

--- non-reactive/src/main/java/org/kurron/non/reactive/gateway/inbound/ResponseDTO.java
+++ non-reactive/src/main/java/org/kurron/non/reactive/gateway/inbound/ResponseDTO.java
@@ -1,8 +1,21 @@
 package org.kurron.non.reactive.gateway.inbound;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Data Transfer Object for the GET request.
+ * Data Transfer Object for the GET request. Here is a good discussion about Java Records: https://dzone.com/articles/jdk-14-records-for-spring-devs
  */
-public record ResponseDTO(@JsonProperty("id") long id,  @JsonProperty("message") String message) { }
+public record ResponseDTO(@JsonProperty("id") long id,  @JsonProperty("message") String message) {
+
+    // do some silly constructor validation
+    public ResponseDTO {
+        if ( 0 == id % 2) {
+            throw new SillyValidationError();
+        }
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "ID cannot be an even number!")
+    static class SillyValidationError extends IllegalArgumentException {}
+}
